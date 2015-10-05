2d8377ee36c12b6ad974800704ec8cdb6e071710
Олег <katexizis94@gmail.com>
2015-10-05 15:51:04 +0300

    1

---
+++ src/Kylinariia.Domain/Service/CommonUserService.cs
@@ -0,0 +1,34 @@
+﻿using Kylinariia.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kylinariia.Domain.Service
+{
+    public class CommonUserService: IService
+    {
+        private List<CommonUser> users = new List<CommonUser>();
+
+        public void Add(object NewUser)
+        {
+            users.Add((CommonUser)NewUser);
+        }
+
+
+        public void Delete(object User)
+        {
+            users.Remove((CommonUser)User);
+        }
+
+        public List<CommonUser> GetAll()
+        {
+            return users;
+        }
+
+        public void Update()
+        {
+            
+        }
+    }
+}
---
+++ src/Kylinariia.Domain/Service/IService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kylinariia.Domain.Service
+{
+    public interface IService
+    {
+         void Add(object a);
+        void Delete(object a);
+        
+        void Update();
+
+    }
+}
