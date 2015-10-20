ba6aed5ecbcaee5839712a6ca027ea3c76d29070
Олег <katexizis94@gmail.com>
2015-10-20 13:14:05 +0300

    1

---
+++ Kulinariia.Domain.Services.Static.Tests/CommonUserServiceTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kulinariia.Domain.Service.Static.Tests
+{
+    [TestClass]
+    public class CommonUserServiceTests
+    {
+        private readonly ICommonUserService
+        [TestMethod]
+        public void AddTest()
+        {
+        }
+    }
+}
---
+++ src/Kylinariia.Domain/Service/ICommonUserService.cs
@@ -0,0 +1,18 @@
+﻿using Kylinariia.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kylinariia.Domain.Service
+{
+    public interface ICommonUserService
+    {
+        
+        void Add(CommonUser item);
+        void Get(CommonUser item);
+        List<CommonUser> Get();
+        void Update(CommonUser item);
+        void Delete(int id);
+    }
+}
